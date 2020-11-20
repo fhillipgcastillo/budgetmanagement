e1bed5ed010ee04eeb4972d3c32779d95c02894e
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2020-11-19 23:52:38 -0400

    Add navigation drawer, add node nvm version used and improve routing

--- README.md
+++ README.md
@@ -3,3 +3,24 @@ Run the App with
 
 
 ##Requirements##
+
+
+# Posible Libraries
+
+React Native Elements - Is best UI toolkit 
+Lottie from RB&B is for animations
+Vector Icons for Icons
+React Native Base is second better ui option
+React Native Paper another ui option with material ui pattern
+
+
+# Next Steps
+## 1. Create Transaccions section and screen
+  1. Register all main accounts clasifications
+  2. Create relationship between accounts
+  3. Register rules about Debe and Haber for each main clasifications
+  4. Create Account state for each account so we can vizualice it
+  5. Create register transaccion screen
+
+## 2. Imrpove budgeting usage
+## 3. Create payment plans for fixed payments
--- src/AppRoute.js
+++ src/AppRoute.js
@@ -1,53 +1,64 @@
 import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
-import ManageAccounts from "./components/ManageAccounts";
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createAppContainer, createSwitchNavigator  } from "react-navigation";
+import ManageAccountsScreen from "./components/ManageAccounts";
 import NewAccount from "./components/NewAccount";
 import AccountDetail from "./components/AccountDetails";
 import NoLogedScreen from "./screens/NoLogedScreen";
 import DashboardScreen from "./screens/DashboardScreen";
 
-const MainNavigation = createStackNavigator(
+const accoutsNavitation = createStackNavigator({
+  ManageAccounts: {
+    screen: ManageAccountsScreen,
+    navigationOptions: {
+      title: "Manage Accounts",
+      headerStyle: {
+        backgroundColor: "#fff"
+      },
+      headerTintColor: "#2c3e50"
+    }
+  },
+  NewAccount: {
+    screen: NewAccount,
+  },
+  AccountDetail: {
+    screen: AccountDetail,
+    navigationOptions: {
+      title: "Details"
+    }
+  }
+});
+
+const mainNvigation = createStackNavigator(
   {
     BashBoard: {
       screen: DashboardScreen,
       navigationOptions: {
-        title: "Weekly Payments Control",
+        title: "Dashboard",
         headerStyle: {
           backgroundColor: "#fff"
         },
         headerTintColor: "#2c3e50"
       },
-    },
-    ManageAccounts: {
-      screen: ManageAccounts,
-      navigationOptions: {
-        title: "Manage Accounts",
-        headerStyle: {
-          backgroundColor: "#fff"
-        },
-        headerTintColor: "#2c3e50"
-      }
-    },
-    NewAccount: {
-      screen: NewAccount,
-    },
-    AccountDetail: {
-      screen: AccountDetail,
-      navigationOptions: {
-        title: "Details"
-      }
     }
-  },
-  { initialRouteName: "BashBoard" }
+  }
+);
+
+const drawerNavigation = createDrawerNavigator (
+  {
+    Dashboard: mainNvigation,
+    Accounts: accoutsNavitation
+  }
 );
 
 const MainSwitchNavigation = createSwitchNavigator(
   {
     noLogedInSplash: NoLogedScreen,
-    MainNavigation: MainNavigation,
+    MainNavigation: drawerNavigation,
   },
   {
-    initialRouteName: "MainNavigation"
+    initialRouteName: "noLogedInSplash"
   }
 );
+
 export default createAppContainer(MainSwitchNavigation);
